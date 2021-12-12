7f723ba291d89a24d23c9112d9b74bf7973a1535
Whittenberg007 <83607510+Whittenberg007@users.noreply.github.com>
2021-12-12 00:07:42 -0800

    Add files via upload

--- AllDevices.py
+++ AllDevices.py
@@ -1,13 +1,15 @@
+
+import os
 import requests
 import json
-#import schedule
-#import time
 
-url = "https://api.meraki.com/api/v1/organizations/"Put Your Organization ID Here"/devices/statuses?"
+API_KEY = os.environ.get('api_key')
+
+url = "https://api.meraki.com/api/v1/organizations/361444/devices/statuses?"
 
 payload={}
 headers = {
-  'X-Cisco-Meraki-API-Key': 'Put Your API Key Here'
+  'X-Cisco-Meraki-API-Key': API_KEY
 }
 
 response = requests.request("GET", url, headers=headers, data=payload)
@@ -16,29 +18,38 @@ allDevices = json.loads(response.text)
 
 #Puts them into a list
 for device in allDevices:
-    #attempts to find all devices that match the MX60 and if they exist print them in a list with the following additional information
-    if device["model"]=="MX60" or device["model"]=="MX64":
+     if device["model"]=="MX60" and device["publicIp"]==device["wan1Ip"]:
         print("Model: {}".format(device["model"]))
         #Prints the name, model, and serial of the device on the network.
         print("Network ID: {} \t Model: {} \t Serial: {}".format(device["networkId"], device["model"], device["serial"]))
         #Prints the status, & lastReportedAt time of the device on the network
         print("Status: {} \t LastReportedAt: {}".format(device["status"], device["lastReportedAt"]))
-        #Prints UsingCellularFailover
-        print("PublicIp: {} \t UsingCellularFailover: {} \n\n".format(device["publicIp"], device["usingCellularFailover"]))
-    #elif device["model"]!="MX60" or device["model"]!="MX60":
-        #print("This Device Is Not A Security Appliance.")
+        #Prints Public IP, & "This Device Is Currently On Wan1Ip."
+        print("Public IP: {} \t ".format(device["publicIp"]) + "\nThis Device Is Currently On Wan1Ip. \n")
 
+     elif device["model"]=="MX60" and device["publicIp"]!=device["wan1Ip"]:
+        print("Model: {}".format(device["model"]))
+        #Prints the name, model, and serial of the device on the network.
+        print("Network ID: {} \t Model: {} \t Serial: {}".format(device["networkId"], device["model"], device["serial"]))
+        #Prints the status, & lastReportedAt time of the device on the network
+        print("Status: {} \t LastReportedAt: {}".format(device["status"], device["lastReportedAt"]))
+        #Prints Public IP, & "This Device Is Currently On Wan1Ip."
+        print("Public IP: {} \t ".format(device["publicIp"]) + "\nThis Device Is NOT Currently On Primary IP, Please Investigate This Device! \n")   
 
-#prints the list of MX60 & MX64 Devices Along with other listed critera
-print(AllDevices)
-#defines the job
-#def apiscript():
-    #Tells the job what to do
-    #print(device)
-#Schedules every day at 5:00 AM to do the job
-#schedule.every(20).seconds.do(apiscript)
-#schedule.every().day.at("05:00").do(apiscript)
-#
-#while True:
-    #schedule.run_pending()
-    #time.sleep(1)
+     elif device["model"]=="MX64" and device["publicIp"]==device["wan1Ip"]:
+        print("Model: {}".format(device["model"]))
+        #Prints the name, model, and serial of the device on the network.
+        print("Network ID: {} \t Model: {} \t Serial: {}".format(device["networkId"], device["model"], device["serial"]))
+        #Prints the status, & lastReportedAt time of the device on the network
+        print("Status: {} \t LastReportedAt: {}".format(device["status"], device["lastReportedAt"]))
+        #Prints Public IP, & "This Device Is Currently On Wan1Ip."
+        print("Public IP: {} \t ".format(device["publicIp"]) + "\nThis Device Is Currently On Wan1Ip. \n")
+
+     elif device["model"]=="MX64" and device["publicIp"]!=device["wan1Ip"]:
+        print("Model: {}".format(device["model"]))
+        #Prints the name, model, and serial of the device on the network.
+        print("Network ID: {} \t Model: {} \t Serial: {}".format(device["networkId"], device["model"], device["serial"]))
+        #Prints the status, & lastReportedAt time of the device on the network
+        print("Status: {} \t LastReportedAt: {}".format(device["status"], device["lastReportedAt"]))
+        #Prints Public IP, & "This Device Is Currently On Wan1Ip."
+        print("Public IP: {} \t ".format(device["publicIp"]) + "\nThis Device Is NOT Currently On Primary IP, Please Investigate This Device! \n")
